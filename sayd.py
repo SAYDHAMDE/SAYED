4fa51e7461b73b35ba433850b19525d8543fda38
SAYDHAMDE <brek1v4@gmail.com>
2024-09-28 04:09:02 +0300

    Update sayd.py

--- sayd.py
+++ sayd.py
@@ -8,7 +8,7 @@ except ModuleNotFoundError as x:
 from concurrent.futures import ThreadPoolExecutor as kil
 
 Id,token = input('\033[2;36m- Enter ID Telegram ~ ادخل ايديك تلجرام : \033[1;97m'),input('\033[2;36m- Enter Token BOT Telegram ~ ادخل توكن البوت : \033[1;97m')
-kopo = requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(Id) + '&text=' + str('- Tool Run • تم تشغيل الاداة ارسل صور الصيد @M_O_L_B '))
+kopo = requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(Id) + '&text=' + str('-  • تم تشغيل الاداة ارسل صور الصيد @M_O_L_B '))
 webbrowser.open('')
 if kopo.status_code == 404:
 	exit('\x1b[91;1m\n - Kilwa • ☒ التوكن او الايدي غلط ☒ ')	
